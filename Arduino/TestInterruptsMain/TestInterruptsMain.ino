1f620e4424c89cb1fccf2fd1611224ef7ebfafc6
g-retchen <ggfzf@mail.umkc.edu>
2017-03-06 14:24:09 -0600

    fixed the interrupt code

---
+++ Arduino/TestInterruptsMain/TestInterruptsMain.ino
@@ -0,0 +1,61 @@
+/* Interrupt on a button
+ * By: Jeffrey Hundstad <we0a@arrl.net> 2015-03-09
+ * Documentation on AttachInterrupt:
+ *  http://arduino.cc/en/Reference/AttachInterrupt 
+
+
+/* PIN definitions */
+const byte led_pin = LED_BUILTIN;  // built in LED
+const byte but_pin = 2;            // Button on pin-2, internal pullup
+const int buttonPin = 3;     // the number of the pushbutton pin
+int buttonState = 0;
+
+/* Volatile variables modified in the ISR */
+volatile boolean led_state = LOW;
+volatile unsigned int last_but_ptime = 0;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(13, OUTPUT); //tester LED
+  
+  pinMode(buttonPin, INPUT);
+  pinMode(led_pin, OUTPUT);
+  pinMode(but_pin, INPUT_PULLUP);
+  /*
+   * attachInterrupt(interrupt, ISR, mode)
+   *  interrupt = [0 == pin 2, 1 == pin 3]
+   *  MODE = [LOW, CHANGE, RISING, FALLING]
+   */
+  attachInterrupt(0, blink, RISING);
+}
+
+/* Show the led_state on the internal LED, it is changed in the ISR */
+void loop()
+{
+    buttonState = digitalRead(buttonPin);
+  // check if the pushbutton is pressed.
+  // if it is, the buttonState is HIGH:
+    if (buttonState == HIGH) {
+    Serial.write('1');
+  } else {  
+    Serial.write('2');
+  }
+    digitalWrite(led_pin, led_state);
+}
+
+
+/* Intended to be attached as an ISR */
+/* Change the led_state but on as fast as PRESS_DELAY */
+const unsigned int PRESS_DELAY = 100; /* milliseconds */
+void blink()
+{
+  unsigned int but_ptime = millis();
+  if ( (but_ptime - last_but_ptime) > PRESS_DELAY) {
+    last_but_ptime = but_ptime;
+    led_state = !led_state;
+    digitalWrite(13, led_state);  //switch this code with GPIO code!
+  }
+}
+
+
--- Arduino/main/main.ino
+++ Arduino/main/main.ino
@@ -1,209 +1,131 @@
-#include <Arduino.h>
+#include <SharpIR.h>
 
-//how to I do an array of four different variables? -- enumerated values
-char current_side;
-
-int current_node;
-
-const int nodes_per_side = 4;
+/* Interrupt on a button
+ * By: Jeffrey Hundstad <we0a@arrl.net> 2015-03-09
+ * Documentation on AttachInterrupt:
+ *  http://arduino.cc/en/Reference/AttachInterrupt 
 
 
-class obstacle_node{
- 
-  //should these be private or public?
-  public:
- 
-  boolean on_tunnel;
-  boolean dead_end;
-  
-  //the value of the die in the cache
-  //value will be zero if there is no die
-  int die_value = 0;
-  
-  //is this node on the perimeter?
-  boolean is_perimeter;
-  
-  //has the robot trasversed this node yet?
-  boolean has_been_here;
-  
- //which side of the course is this node on? Only relevant if
- //is_perimeter == true
-  char which_side;
-};
+/* PIN definitions */
+const byte led_pin = LED_BUILTIN;  // built in LED
+const byte but_pin = 3;            // Button on pin-2, internal pullup
+const int buttonPin = 2;     // the number of the pushbutton pin
+int buttonState = 0;
+
+/* Volatile variables modified in the ISR */
+volatile boolean led_state = LOW;
+volatile unsigned int last_but_ptime = 0;
+
+byte number = 0;
+int state = 0;
+
+//IR Sensor
+#define ir A0
+#define model 20150
+//boolean done=false;
+SharpIR sharp(ir, 25, 93, model);
 
 
-void searchPerimeter(obstacle_node *a);
- 
-//update node according to course characteristics
-void updateNode(obstacle_node *a);
-void nextNode();
-boolean is_searching;
+/*
+button press code:
+1 - start button has been pressed
+*/
 
-
-
-
-//do I need to include function headers for Arduino?
-
-void setup(){
-  
+void setup()
+{
   Serial.begin(9600);
-  //while(!Serial);
-  
-  int number_of_nodes = (nodes_per_side*nodes_per_side);
-  //create array of Obstacle_nodes
-  //need to initiate all of these values to 0.
-  obstacle_node course_nodes[number_of_nodes];
-  
-  
-  //mark "is_searching" as true
-  is_searching = true;
-  
-  //initialize first side to 0
-  current_side = 's';
-  
-  //the first node in the grid will always be 1
-  //set current_node to 1 -- the first corner of the course
-  current_node = 1;
-  
-  
-  //add function: Detect walls
-  //add function: face correct direction
-  
-  //gather data from the perimeter
-  searchPerimeter(course_nodes);
-  
-  Serial.print("Perimeter search complete!");
+  pinMode(buttonPin, INPUT);
+  pinMode(led_pin, OUTPUT);
+  pinMode(but_pin, INPUT_PULLUP);
+  pinMode (ir, INPUT);
+
   /*
-  //find tunnel beginning and go there
-  for(int i = 1; i < number_of_nodes; i++){
-    if(course_nodes[i].on_tunnel){
-      //add function: go to beginning of tunnel
-      break; //exit the for loop
-    }  
-  }*/
-  while(1);
-  
-
-}
-
-
-void loop(){
-  while(true);
-  
-}
-
-
-void searchPerimeter(obstacle_node *a){
-  //search the perimeter for as long as we haven't returned to the first corner.
-  //if we are back at the first corner, exit loop
-  while(is_searching){
-  //Is there a die cache on this node?
-  //If yes, check die cache and update LED
-  
-  //get and save information and update node data
-  updateNode(a);
+   * attachInterrupt(interrupt, ISR, mode)
+   *  interrupt = [0 == pin 2, 1 == pin 3]
+   *  MODE = [LOW, CHANGE, RISING, FALLING]
+   */
+  attachInterrupt(1, blink, RISING);
   
   
-  //go to next node
-  next_node();
-  
+  //wait for the button to be pressed (we still need to buy an inverted schmitt trigger)
+  while (buttonState != HIGH) {
+    //  Serial.write("can't start car until button is pressed!");
+      buttonState = digitalRead(buttonPin);
   }
-  return;
+  Serial.write('1');
 }
 
-
-void updateNode(obstacle_node *a){
+/* Show the led_state on the internal LED, it is changed in the ISR */
+void loop()
+{
+  // digitalWrite(led_pin, led_state);    
+    
+  //look for serial from the pi
+  //get the values from the sensors
+  //send the values to the raspberry pi (determine beforehand where you are on the track -- are you on a tunnel edge?)
+  //update the 7x7 LED display
   
-
-  int not_real;
-
-      
-    //used for testing
-   Serial.println("input a letter for the direction");
-  //wait until user input values
+  //wait until Pi is ready to communicate
   while(!Serial.available());
-//    Serial.println("input a letter for the direction");
-    not_real = Serial.read();
-    a[current_node].which_side == not_real;
-    Serial.println(not_real);
 
-
-  //is there a die?
-  //get value from die
-  
-  //is there a tunnel underneath?
-  
-  //is there a wire in the tunnel?
-  
-  //is the tunnel a dead end? -- I might not want to actually add this function.
-  
-  return;
-}
-
-void next_node(){
-  
-  
-  //   Serial.println("5");
+   number = Serial.read();
+   if (number == 'd')
+     Serial.write('z');
+   //Serial.write(number);
+ //  Serial.print("character recieved");
+  // Serial.print(number);
+//   if (number != 'd')
+  //   while(1);
+   //get and send values to the pi
+   boolean emf_values = isEMF();
+   boolean obj_in_front = objInFront();
+   
+   if(isEMF())
+     Serial.write('E');
+   else
+     Serial.write('F');
      
-  //turn left if avc is at a corner
-  switch(current_node){
-    case(nodes_per_side):           
-            current_side = 'e';
-            //add function: turn left
-            break;
-    case(nodes_per_side*nodes_per_side):           
-            current_side = 'n';
-            //add function: turn left
-            break;
-    case(nodes_per_side*nodes_per_side + 1 - nodes_per_side):           
-            current_side = 'w';
-            //add function: turn left
-            break;
-    case(1 + nodes_per_side):           
-            current_side = 's';
-            //add function: turn left
-            //add function: go straight
-            is_searching = false;
-            break;
-    default:
-        //output error message
-        break;
-  }
-
-  
-  //add function: go straight one node
-
-  
-     switch(current_side){
-      case 's':
-        Serial.println("current_side");
-        current_node++;
-            Serial.println("here");
-            break;
-      case 'e':
-        current_node += nodes_per_side;
-        Serial.println("current_sidee");
-            break;
-      case 'n':
-        current_node--;
-            break;
-      case 'w':
-        current_node = current_node - nodes_per_side;
-            break;
-      default:
-        //output error message
-        Serial.println("current_sided");
-        break;
-      }
-       
-          Serial.println("current_node");
-               Serial.println(current_node);
-    //           
-                         Serial.println("current");
-  
-  return;
-
+   if(objInFront())
+     Serial.write('G');
+   else
+     Serial.write('H');
+     
+   //Serial.write(emf_values);
+   //Serial.write(obj_in_front);
+   
+   //update the 7x7 grid
+ 
+   digitalWrite(led_pin, led_state);
 }
 
 
-//create error functions below:
+/* Intended to be attached as an ISR */
+/* Change the led_state but on as fast as PRESS_DELAY */
+const unsigned int PRESS_DELAY = 100; /* milliseconds */
+void blink()
+{
+  unsigned int but_ptime = millis();
+  if ( (but_ptime - last_but_ptime) > PRESS_DELAY) {
+    last_but_ptime = but_ptime;
+    led_state = !led_state;
+  }
+}
+
+//determine if there is an EMF signal under the robot
+boolean isEMF(){
+  //if on EMF tunnel, 
+  //return true;
+  //else
+  //return false;
+  return true;
+}
+
+boolean objInFront(){
+  int dis = sharp.distance();
+  
+  if(dis > 100)
+    return true;
+   else 
+     return false;
+}
+

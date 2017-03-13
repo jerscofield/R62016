51cfd8a276b3f546d58e3cd390703929d596e418
g-retchen <ggfzf@mail.umkc.edu>
2017-03-13 04:00:06 -0500

    navigation code now works

--- Python/Robot/grid.py
+++ Python/Robot/grid.py
@@ -4,10 +4,12 @@
 class ObstacleNode:
     def __init__(self, node_number):
         self.die_value = 0
-        self.is_perimeter = 0
-        self.has_been_here = 0
+        self.is_perimeter = False
+	self.is_inner_perimeter = False
+        self.is_corner = False
+	self.is_inner_corner = False
+        self.has_been_here = False
         self.which_side = '*'
-        self.is_searching = 0
         #self.which_quadrant
         self.node_number = node_number
         self.is_tunnel = 0
@@ -19,65 +21,121 @@ class ObstacleNode:
 
 class Grid(object):
     def __init__(self, number_of_nodes):
-        self.current_node = 1
+        self.current_node = int(1)
         self.is_searching = False
         self.is_grid_search = False
         self.current_side = 's'
         self.number_of_nodes = number_of_nodes
-        self.is_corner = 0
         self.orientation = 'e'
         self.grid = []
-        for i in range(number_of_nodes):
-            self.grid.append(ObstacleNode(i + 1))
+        for i in range(number_of_nodes + 1):
+            self.grid.append(ObstacleNode(i))
             #Determine what direction we are facing
             #if e  +1,  if n +7,  if s -7, if w -1.
+
 
     def __getitem__(self,index):
         return self.grid[index]
 
+
     def __setitem__(self,index,value):
         self.grid[index] = value
+
 
     def get_value(self,which_node):
         return self.grid[which_node].node_number, self.grid[which_node].which_side
 
+
+    def initialize(self):
+	self.orientation = 'e'
+        for i in range(50):
+	    if i%7 == 6 or i%7 == 2:
+		self.grid[i].is_inner_perimeter = True
+            if i%7 == 0 or i%7 == 1 or i > 42 or i < 8:
+                self.grid[i].is_perimeter = True
+                self.grid[i].cache_present = 1
+		self.grid[i].is_inner_perimeter = False
+            if i == 1 or i == 7 or i == 43 or i == 49:
+                self.grid[i].is_corner = True
+                self.grid[i].cache_present = 0
+	    if i == 9 or i == 13 or i == 41 or i == 37:
+		self.grid[i].is_inner_corner = 1
+		self.grid[i].is_inner_perimeter = True
+
+                
     def update_node(self, is_tunnel):
-        course_nodes[current_node].is_tunnel = is_tunnel
+        course_nodes[self.current_node].is_tunnel = is_tunnel
        #figure out later
 
-    def next_node(self):
-        self.is_corner = 1  # will this set corner every time?
+    #turn types:
+    #0: go straight
+    #1: go straight, turn left
+    #2: go straight, turn left
+    #3: stop
+    #4: turn 180 degrees
+    def next_node_perim(self):
+	#we are right before the corner
+        if self.grid[self.current_node + self.increment_node()].is_corner == True:
+	    self.current_node = self.current_node + self.increment_node()
+	    self.orientation = self.change_orientation('l')
+            turn_type = 1 #go stright 1, then turn left. Make sure to update orientation
+	elif self.current_node == 15:
+            turn_type = 1 #go stright 1, then turn left. Make sure to update orientation
+	    self.is_searching = False
+            self.current_node = self.current_node + self.increment_node() 
+	    self.orientation = self.change_orientation('l')
+	else:
+	    turn_type = 0	#go straight 1
+            self.current_node = self.current_node + self.increment_node()	    
+	return turn_type
 
-        # turn left if Berto is at a corner
-        if self.current_node == 7:
-            self.current_side = 'e'
-            self.current_node = (self.current_node + 1)
-        elif self.current_node == 49:
-            self.current_side = 'n'
-        elif self.current_node == 43:
-            self.current_side = 'w'
-            self.is_corner = 1
-        elif self.current_node == 8:
-            self.current_side = 'd'
-            self.is_searching = 0
-            self.is_corner = 3
-        elif self.current_node == 6:    #robot is right before corner
-            self.is_corner = 2
-        elif self.current_node == 42:
-            self.is_corner = 2
-        elif self.current_node == 44:
-            self.is_corner = 2
-        else:
-            self.is_corner = 0
 
-        if self.current_side == 's':
-            self.current_node = (self.current_node + 1)
-        elif self.current_side == 'e':
-            self.current_node = (self.current_node + 7)
-        elif self.current_side == 'n':
-            self.current_node = (self.current_node - 1)
-        elif self.current_side == 'w':
-            self.current_node = (self.current_node - 7)
+    def increment_node(self):
+        if self.orientation == 's':
+  	    increment = int(-7)
+        elif self.orientation == 'e':
+            increment = int(1)
+        elif self.orientation == 'n':
+            increment = int(7)
+        elif self.orientation == 'w':
+            increment = int(-1)
+	else:
+	    increment = int(0)
+	    print ("error here. current orientation is: ", self.orientation)
+	return increment
+
+
+    def change_orientation(self, turnDirection):
+	if turnDirection == 'r':
+		if self.orientation == 'n':
+			orientation = 'e'
+		if self.orientation == 'e':
+			orientation = 's'
+		if self.orientation == 's':
+			orientation = 'w'
+		if self.orientation == 'w':
+			orientation = 'n'
+	elif turnDirection == 'l':
+		if self.orientation == 'n':
+			orientation = 'w'
+		if self.orientation == 'e':
+			orientation = 'n'
+		if self.orientation == 's':
+			orientation = 'e'
+		if self.orientation == 'w':
+			orientation = 's'
+	#total orientation change
+	elif turnDirection == 't':
+		if self.orientation == 'n':
+			orientation = 's'
+		if self.orientation == 'e':
+			orientation = 'w'
+		if self.orientation == 's':
+			orientation = 'n'
+		if self.orientation == 'w':
+			orientation = 'e'
+	return orientation
+        
 
         ###notes:  Jeremy
         ##if self.current_node <= 7:       # south edge
@@ -93,51 +151,40 @@ class Grid(object):
         #if self.current_node % == (1|7|43|49): self.is_perimeter=1
         #corner node numbers  1, 7, 43, 49
 
-        return self.is_corner
         # if self.current.node%7== (0 | 1)  #  modulus equals 1 or 0.
 
     #turn types:
     #0: go straight
-    #1: turn left, go straight
-    #2: turn left, go straight, turn left
-    #3: turn right, go straight, turn right
-    #4: stop
+    #1: go straight, turn left
+    #2: go straight, turn right
+    #3: stop
+    #4: turn 180 degrees
     def next_node_grid(self):
-                #if we are on the westmost edge of the inner grid
+        #if we are at the final node in the search
+	
         if self.current_node == 41:
-            self.is_searching = 0
-            turnType = 4
-        elif self.current_node == 8:
-            self.current_node = self.current_node + 1
-            self.orientation = 'e'
-            turnType = 1
-        elif self.current_node == 9:
-            self.current_node = self.current_node + 1
-            turnType = 0
-        #if we are on the westmost side of the inner grid
-        elif self.current_node % 7 == 1 and self.orientation != 'e':
-            self.current_node = self.current_node + 7
-            self.orientation = 'e'
-            self.in_turn = 0
+            self.is_searching = False
             turnType = 3
-        #if we are on the eastmost side of the inner grid
-        elif self.current_node % 7 == 6 and self.orientation !=  'w':
-            self.current_node = self.current_node + 7
-            self.orientation = 'w'
-            self.in_turn = 0
-            turnType = 2  
-        elif self.orientation == 'e':        
-            self.current_node = self.current_node + 1
-            self.in_turn = 1
-            turnType = 0   
-        elif self.orientation == 'w':
-            turnType = 0             #go straight
-            self.current_node = self.current_node - 1
-            self.in_turn = 1
-        elif self.current_node == 41:
-            self.is_searching = 0
-            turnType = 4
-            
-        return turnType
+	#if we are at the first node in the search
+        elif self.current_node == 8:
+            self.current_node = self.current_node + self.increment_node()
+            turnType = 0
+	#if we are at or right before the inner perimeter, on east side
+	elif (self.grid[self.current_node + 2].is_perimeter == True and self.orientation == 'e') or (self.grid[self.current_node + 1].is_perimeter == True and self.orientation == 'n'):
+	    print ("We got to this point!!!")
+	    self.current_node = self.current_node + self.increment_node()
+	    self.orientation = self.change_orientation('l')
+	    turnType = 1
+	#if we are at or right before the inner perimeter, on west side
+	elif (self.grid[self.current_node - 2].is_perimeter == True and self.orientation == 'w') or (self.grid[self.current_node - 1].is_perimeter == True and self.orientation == 'n'):
+	    self.current_node = self.current_node + self.increment_node()
+	    self.orientation = self.change_orientation('r')
+            turnType = 2
+	#no turns ahead, go straight
+	else:
+	    self.current_node = self.current_node + self.increment_node()
+	    turnType = 0
+
+	return turnType
 
     #def return_to_start(self):
--- Python/Robot/main.py
+++ Python/Robot/main.py
@@ -1,30 +1,103 @@
 from __future__ import print_function
 from __future__ import division
-from binascii import hexlify
+#from binascii import hexlify
 
 import grid
-import rotationMotorTest
+#$$$$$$$$$$$$$import rotationMotorTest
+#$$$$$$$$$$$$$$$$$$$$import RPi.GPIO as GPIO
 import serial
-
 import time
-from BrickPi import *  # import BrickPi.py file to use BrickPi operations
-from MultiMotorDriving import *  # So can do precision motor rotations
+#$$$$$$$$$$$$$$$$$$import os
+
+#$$$$$$$$$$$$$$$from BrickPi import *  # import BrickPi.py file to use BrickPi operations
+#$$$$$$$$from MultiMotorDriving import *  # So can do precision motor rotations
+
+#function for shutdown pi when red button is pressed
+#$$$$$$$$$def Shutdown(channel):
+   #$$$$$$$$$ GPIO.cleanup()
+    #$$$$$$$$$$$$$os.system("sudo shutdown -h now")
+
+
+#0 go straight
+#1 go straight, turn left
+#2 stop
+#call motor object for controling the motor
+#perimeter search. will continue to run as long as the last node hasn't been reached
+def PerimeterSearch(course_nodes):
+    course_nodes.is_searching = 1
+    #perimeter search. will continue to run as long as the last node hasn't been reached
+    while course_nodes.is_searching == 1:
+        print ("Perimeter Search", course_nodes.current_node)
+	#update sensor values
+        #$$$$$ser.write('G');
+        #get EMF value from Arduino
+        #$$$$$$4EMF = ser.read()
+        #print (EMF)
+        
+        #$$$$$$ir_sensor = ser.read()
+        #print (ir_sensor)
+        
+	action_to_take = course_nodes.next_node_perim()
+        #action_to_take = course_nodes.next_node()
+        
+        if action_to_take == 0:
+            inp = 'w'       #robot isn't at a corner. go straight.
+        elif action_to_take == 1:
+            inp = 'i'       #robot is right before the corner. go straight, turn left (will need to change code to do this)
+        elif action_to_take == 2:
+            inp = 's'       #robot is at a corner. does a half turn onto the next node
+            
+       #$$$$$$$$$ motor.move_bot(inp)  # Send command to move the bot
+        time.sleep(.5)  # sleep for 10 ms
+
+#0 go straight
+#1 turn left, go straight
+#2 turn left, go straight, turn left
+#3 turn right, go straight, turn right
+#4 stop
+def GridSearch(course_nodes):
+    course_nodes.is_searching = 1
+    while course_nodes.is_searching == 1:
+   	print ("Grid Search", course_nodes.current_node, " self.orientation: ", course_nodes.orientation)
+	#update sensor values
+        action_to_take = course_nodes.next_node_grid()
+        if action_to_take == 0:
+            inp = 'w'       #robot isn't at a corner. go straight.
+        elif action_to_take == 1:
+            inp = 'i'       #robot is at beginning of perimeter search. needs to turn into grid
+        elif action_to_take == 2:
+            inp = 'j'       #robot is at easternmost inner grid
+        elif action_to_take == 3:
+            inp = 's'       #robot is at westernmost inner grid
+        elif action_to_take == 4:
+            inp = 't'       #stop
+       # motor.move_bot(inp)  # Send command to move the bot
+        time.sleep(.5)  # sleep for 10 ms
+
+#def UpdateValues():
+
+
+#setup interrupts for stop push button
+#$$$$$$4GPIO.setmode(GPIO.BCM)
+#$$$$$$$$$$$$$$GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+#$$$$$$$$$$GPIO.add_event_detect(11, GPIO.FALLING, callback = Shutdown, bouncetime = 2000)
 
 
 def main():
-    number_of_nodes = 49
+    number_of_nodes = 100
     course_nodes = grid.Grid(number_of_nodes)
-    left_motor = PORT_B
-    right_motor = PORT_A
-    motor = rotationMotorTest.MotorControls(left_motor, right_motor)  
+    #$$$$$$$4left_motor = PORT_B
+    #$$$$$$$$$$right_motor = PORT_A
+    #$$$$$$$$$$motor = rotationMotorTest.MotorControls(left_motor, right_motor)
 
-  #  ser = serial.Serial('/dev/ttyACM0',9600)
+   
+   # ser = serial.Serial('/dev/ttyACM1',9600)
   #  has_been_pressed = 0
   #  while has_been_pressed == 0:
   #      read_serial=ser.read()
   #      if (read_serial == '1'):
    #         has_been_pressed = 1
-    course_nodes[course_nodes.current_node].cache_present
+ #   course_nodes[course_nodes.current_node].cache_present
    # time.sleep(20)
       
    #  BrickPiSetup()  # Setup the serial port for communication (***NB*** APP MUST BE RUN IN SUDO MODE ***NB***)
@@ -36,53 +109,24 @@ def main():
    # BrickPiSetupSensors()  # Send the properties of sensors to BrickPi
    # BrickPi.Timeout = 30000  # So motors won't stop cause of lack of contact (30 seconds)
    # BrickPiSetTimeout()  # (BrickPi's default is 250 msec (really meeses with motor reliability))
+	
+    course_nodes.initialize()
+    print ("course_nodes[13].is_perimeter:", course_nodes[13].is_perimeter)
+    PerimeterSearch(course_nodes)
+    #&&&&&&&&&&&77PerimeterSearch(course_nodes, motor)
+    #GridSearch(course_nodes, motor)
+    GridSearch(course_nodes)
 
-    #call motor object for controling the motor
 
-    course_nodes.is_searching = 1
-    #perimeter search. will continue to run as long as the last node hasn't been reached
-    while course_nodes.is_searching == 1:
-        print ("Perimeter Search", course_nodes.current_node)
-        course_nodes[course_nodes.current_node].is_perimeter = 1
-        action_to_take = course_nodes.next_node()
-        
-        if action_to_take == 0:
-            inp = 'w'       #robot isn't at a corner. go straight.
-        elif action_to_take == 2:
-            inp = 'n'       #robot is right before the corner. does a half turn onto the corner
-        elif action_to_take == 1:
-            inp = 'm'       #robot is at a corner. does a half turn onto the next node
-
-      #  motor.move_bot(inp)  # Send command to move the bot
-        time.sleep(.5)  # sleep for 10 ms
-    
-    course_nodes.is_searching = 1
-    while course_nodes.is_searching == 1:
-        print (course_nodes.current_node)
-        turnType = course_nodes.next_node_grid()
         #if turnType == 0:
             #motor.move_bot('w')
-    print ("done with grid search!")
 
-    
-    #turn right
-   # motor.move_bot('w')     #go straight 6
-   # motor.move_bot('w')     #go straight 6
-   # motor.move_bot('w')     #go straight 6
-   # motor.move_bot('w')     #go straight 6
-   # motor.move_bot('w')     #go straight 6
-   # motor.move_bot('w')     #go straight 6
-    #turn right
-   # motor.move_bot('w')     #go straight 6
-   # motor.move_bot('w')     #go straight 6
-   # motor.move_bot('w')     #go straight 6
-   # motor.move_bot('w')     #go straight 6
-   # motor.move_bot('w')     #go straight 6
-   # motor.move_bot('w')     #go straight 6
-    #turn 180 degrees
+
+    #return_to_start
 
     while 1:
         time.sleep(.01)
+	print ("done")
 
 if __name__ == "__main__":
     main()
